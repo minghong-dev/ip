20d8ef2b13150a907a73e5362fe3e0e26b623dc0
Ming Hong <tangmh17@gmail.com>
2026-08-19 16:34:20 +0800

    Rename the chatbot

---
+++ src/main/java/NiuLai.java
@@ -0,0 +1,7 @@
+public class NiuLai {
+    public static void main(String[] args) {
+        String banner = "|\\ | | |  | |     /\\  |\n"
+                + "| \\| | \\__/ |___ /~~\\ |\n";
+        System.out.println(banner);
+    }
+}
