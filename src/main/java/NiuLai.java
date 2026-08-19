b55908170a84c4a99f97ac6264937d22775e289d
Ming Hong <tangmh17@gmail.com>
2026-08-19 16:46:18 +0800

    Update the initial code to greet the user and exit.

--- src/main/java/NiuLai.java
+++ src/main/java/NiuLai.java
@@ -3,5 +3,13 @@ public class NiuLai {
         String banner = "|\\ | | |  | |     /\\  |\n"
                 + "| \\| | \\__/ |___ /~~\\ |\n";
         System.out.println(banner);
+
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm NiuLai!");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope not to see you again.");
+        System.out.println(line);
     }
 }
