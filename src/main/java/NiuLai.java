a04a7120d08131fc2740f19d01501dd408495b81
Ming Hong <tangmh17@gmail.com>
2026-08-19 16:56:45 +0800

    Add support for echo

--- src/main/java/NiuLai.java
+++ src/main/java/NiuLai.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Runs the NiuLai command-line chatbot.
+ */
 public class NiuLai {
     public static void main(String[] args) {
         String banner = "|\\ | | |  | |     /\\  |\n"
                 + "| \\| | \\__/ |___ /~~\\ |\n";
         System.out.println(banner);
 
-        String line = "____________________________________________________________";
+        String line = "    ____________________________________________________________";
         System.out.println(line);
-        System.out.println("Hello! I'm NiuLai!");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope not to see you again.");
-        System.out.println(line);
+        System.out.println("     Hello! I'm NiuLai!");
+        System.out.println("     What can I do for you?");
+        System.out.println(line + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            System.out.println(line);
+
+            if (command.equals("bye")) {
+                System.out.println("     Bye. Hope not to see you again.");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println("     " + command);
+            System.out.println(line + "\n");
+        }
     }
 }
